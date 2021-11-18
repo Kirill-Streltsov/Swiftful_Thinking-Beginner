6f4483561343368edc6b5a606aa46ffb06e069d9
Kirill Streltsov <krlsrlsv@gmail.com>
2021-11-18 17:58:02 +0100

    Stepper

---
+++ Swiftful Thinking - Beginner/Steppers.swift
@@ -0,0 +1,20 @@
+//
+//  Steppers.swift
+//  Swiftful Thinking - Beginner
+//
+//  Created by Кирилл Стрельцов on 18.11.2021.
+//
+
+import SwiftUI
+
+struct Steppers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Stepper_Previews: PreviewProvider {
+    static var previews: some View {
+        Steppers()
+    }
+}
