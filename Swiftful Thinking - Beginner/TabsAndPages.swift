95f2136d61bf94b06318522e520da724509fab5e
Kirill Streltsov <krlsrlsv@gmail.com>
2021-11-20 17:29:44 +0100

    Tabs and Pages

---
+++ Swiftful Thinking - Beginner/TabsAndPages.swift
@@ -0,0 +1,20 @@
+//
+//  TabsAndPages.swift
+//  Swiftful Thinking - Beginner
+//
+//  Created by Кирилл Стрельцов on 20.11.2021.
+//
+
+import SwiftUI
+
+struct TabsAndPages: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabsAndPages_Previews: PreviewProvider {
+    static var previews: some View {
+        TabsAndPages()
+    }
+}
