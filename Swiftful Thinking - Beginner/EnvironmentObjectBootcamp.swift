b4f642e8882503614b2464898c6a8bbbacf3fb38
Kirill Streltsov <krlsrlsv@gmail.com>
2021-11-27 21:32:48 +0100

    @EnvironmentObject

---
+++ Swiftful Thinking - Beginner/EnvironmentObjectBootcamp.swift
@@ -0,0 +1,94 @@
+//
+//  EnvironmentObjectBootcamp.swift
+//  Swiftful Thinking - Beginner
+//
+//  Created by Кирилл Стрельцов on 27.11.2021.
+//
+
+import SwiftUI
+
+class EnvironmentViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+    
+    init() {
+        getData()
+    }
+    
+    func getData() {
+        dataArray.append(contentsOf: ["iPhone", "iPad", "iMac", "Apple Watch"])
+    }
+}
+
+struct EnvironmentObjectBootcamp: View {
+    
+    @StateObject var viewModel: EnvironmentViewModel = EnvironmentViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.dataArray, id: \.self) { item in
+                    NavigationLink {
+                        DetailView(selectedItem: item)
+                    } label: {
+                        Text(item)
+                    }
+                }
+            }
+            .navigationTitle("iOS Devices")
+        }
+        .environmentObject(viewModel)
+    }
+}
+
+struct DetailView: View {
+    
+    let selectedItem: String
+    
+    var body: some View {
+        ZStack {
+            Color.orange.ignoresSafeArea()
+            NavigationLink {
+                FinalView()
+            } label: {
+                Text(selectedItem)
+                    .font(.headline)
+                    .foregroundColor(.orange)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    .cornerRadius(30)
+            }
+        }
+    }
+}
+
+struct FinalView: View {
+    
+    @EnvironmentObject var viewModel: EnvironmentViewModel
+        
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                colors: [.red, .blue],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+                .ignoresSafeArea()
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(viewModel.dataArray, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct EnvironmentObjectBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        EnvironmentObjectBootcamp()
+    }
+}
